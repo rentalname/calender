862122611c12fc321c32f254bef57a3507f3d5e8
rentalname <agenda21st@gmail.com>
2013-02-24 00:20:07 +0900

    my first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Calender::Application.config.secret_token = '127c434f2b4d62686acbd6a8ed41defcaefe9cc1dd296419e546678679b752727af795f0851573c0c05e57e9bca50771daaa20c451c07e7c378b450a9fde9f18'
